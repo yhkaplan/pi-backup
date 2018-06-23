7f3991d4a31db47271bdf1d8bfc404038a545052
Joshua Kaplan <yhkaplan@gmail.com>
2018-06-23 18:10:30 +0900

    Fixed file naming

--- pi-backup.sh
+++ pi-backup.sh
@@ -2,7 +2,7 @@
 # Referenced https://raspberrypi.stackexchange.com/questions/72044/is-there-a-way-to-compress-a-backup-of-the-rpi-because-the-backup-has-empty-spac
 
 OUTPUT_DIR=~/
-OUTPUT_NAME="pi_backup"
+OUTPUT_NAME="raspbian_backup"
 
 # Looks for disk w/ Linux parition
 pi_disk=$(diskutil list | grep "Linux" | sed 's/.*\(disk[0-9]\).*/\1/' | uniq)
@@ -20,4 +20,4 @@ echo "Backing up... (ctrl+T for progress)"
 time sudo dd if=/dev/r$pi_disk bs=4m | gzip -9 > $OUTPUT_DIR/$OUTPUT_NAME
 
 echo "Adding date to name"
-mv -n $OUTPUT_DIR/$OUTPUT_NAME $OUTPUT_DIR/$OUTPUT_NAME-$(date +%Y-%m-%d).img.gz
+mv -n $OUTPUT_DIR/$OUTPUT_NAME $OUTPUT_DIR/$OUTPUT_NAME_$(date +%Y-%m-%d).img.gz
