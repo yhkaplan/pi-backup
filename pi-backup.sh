f795f600f0cd5b6c8b14fe8b634a157cae2efcf5
Joshua Kaplan <yhkaplan@gmail.com>
2018-06-23 16:48:51 +0900

    First commit

---
+++ pi-backup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Referenced https://raspberrypi.stackexchange.com/questions/72044/is-there-a-way-to-compress-a-backup-of-the-rpi-because-the-backup-has-empty-spac
+
+OUTPUT_DIR = ~/
+OUTPUT_NAME = "pi_backup"
+
+# Looks for disk w/ Linux parition
+pi_disk="diskutil list | grep "Linux" | sed 's/.*\(disk[0-9]\).*/\1/' | uniq`
+
+if [ $? ]; then
+    echo "Disk is $pi_disk"
+    echo "Output dir is $OUTPUT_DIR"
+else
+    echo "Disk not found"
+    exit(1)
+fi
+
+diskutil unmountDisk /dev/$pi_disk
+echo "Backing up... (ctrl+T for progress)"
+time sudo dd if=/dev/r$pi_disk bs=4m | gzip -9 > $OUTPUT_DIR/$OUTPUT_NAME
+
+echo "Adding date to name"
+mv -n $OUTPUT_DIR/$OUTPUT_NAME $OUTPUT_DIR/$OUTPUT_NAME$(date +%Y-%m-%d).img.gz
+
